18158e5ac66c3c562dd822dad6f91301ca7f1198
ThienLam <thien.lam@eastagile.com>
2022-03-15 13:38:25 +0700

    Use TEMPLATES_GLOB env

--- src/templates.rs
+++ src/templates.rs
@@ -1,9 +1,11 @@
 use tera::Tera; 
 use lazy_static::lazy_static;
+use std::env;
 
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
-        let mut tera = match Tera::new("templates/**/*") {
+        let glob = env::var("TEMPLATES_GLOB").expect("TEMPLATES_GLOB must be set");
+        let mut tera = match Tera::new(&glob) {
             Ok(t) => t,
             Err(e) => {
                 println!("Parsing error(s): {}", e);
